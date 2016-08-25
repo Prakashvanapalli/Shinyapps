5662253ad8b1ffde15e00411ffd27cb89b058bd1
PrakashR <vanapaliprakash@gmail.com>
2016-08-26 04:08:14 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,9 +8,10 @@ shinyUI(fluidPage(
                  textInput("file", "File:"),
                  actionButton("browse", "Browse"),
                  actionButton("upload", "Upload Data"),
+                 radioButtons("sub_group","Sub_Group",c("A3","B3","C3","D3")),
                  selectInput("var", 
                              label = "Choose the algorithm",
-                             choices = c("Logistic Regression","Decision Trees","GBM","Random Forest","XGBoost"))
+                             choices = c("Logistic Regression","Decision Trees","GBM","Random Forest"))
                  ),
     mainPanel(h1("Model Output"),
               h3(textOutput("nrows")),
